141128e266e9081f652a40984ed12da716c40e88
SMiller <miller8547@gmail.com>
2022-07-10 13:53:09 -0500

    Initial Commit

---
+++ DonkeyAndElephant/DonkeyAndElephant/MODEL/TweetManager.swift
@@ -0,0 +1,19 @@
+//
+//  TweetManager.swift
+//  DonkeyAndElephant
+//
+//  Created by Sterling Miller on 7/9/22.
+//
+
+import Foundation
+
+
+protocol TweetManagerDelegate{
+    func didReturnTweet()
+    func errorReturningTweet(_ error: Error)
+}
+
+class TweetManager{
+    
+    
+}//EOC
